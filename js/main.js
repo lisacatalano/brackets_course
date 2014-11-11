c94187554094ca89c3e1255f64171a6178f3db7d
Lisa Catalano <@lcatdesigns (twitter)>
2014-11-11 11:33:14 -0600

    Navigation, Adding Javascript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
-
+(function ($) {
+  // Hide all shortcuts and notes
+  var hideAll = function() {
+    $('.shortcuts').hide();
+    $('.notes').hide();
+  };
+ 
+  // animate the platform buttons when user chooses one before taking them page. 
+  $('.platform').on('click', function(e) {
+    var url = this.href;
+    var $target = $(this).parent();
+    e.preventDefault();
+    
+    $target.addClass('active').siblings().hide();
+    $target.animate({
+      width: '90%', 
+      height: '500px', 
+      opacity: 0
+    }, 1000, function() {
+      window.location = url;
+    });
+  });
+ 
+  // toggle display of shortcuts when clicking on header
+  $('h2').on('click keypress', function(){
+    $(this).parent().find('.shortcuts, .notes').slideToggle();
+  });
+    
+  hideAll();
+  
+}(jQuery));
